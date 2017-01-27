ef88672bee892ab48ddfdc647f724df637dcb53a
James Boullion <jboullion83@gmail.com>
2017-01-26 21:58:39 -0600

    Initial

---
+++ src-files/scripts/controller.js
@@ -0,0 +1,32 @@
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			recipes: [],
+		}
+	}
+
+	componentWillMount(){
+
+	}
+
+	handleClick(){
+
+	}
+
+	render() {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
+
+// ========================================
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
