70393af6d2efad41993a788466451c265c7243a2
James Boullion <jboullion83@gmail.com>
2017-02-11 14:12:22 -0600

    Moved the html content into react
    I don't like how I styled all this, but I have learned a lot about what
    works well with react and what does not.
    
    This isn't a style test, but a React test so I am good with what is
    there, even if it is ugly.

--- public/scripts/live.js
+++ public/scripts/live.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -7,6 +7,127 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function Ingredient(props) {
+	var ingredientList = [];
+	for (var i = 0; i < props.recipe.ingredients.length; i++) {
+		ingredientList.push(React.createElement(
+			"li",
+			{ key: i },
+			props.recipe.ingredients[i]
+		));
+	}
+
+	return React.createElement(
+		"table",
+		{ className: "table table-striped" },
+		React.createElement(
+			"tbody",
+			null,
+			React.createElement(
+				"tr",
+				{ role: "button", className: 'recipe-' + props.recipe.id },
+				React.createElement(
+					"td",
+					{ className: "index", "data-toggle": "collapse", href: '#recipe-' + props.recipe.id, "aria-expanded": "false" },
+					props.index
+				),
+				React.createElement(
+					"td",
+					{ className: "name", "data-toggle": "collapse", href: '#recipe-' + props.recipe.id, "aria-expanded": "false" },
+					props.recipe.name
+				),
+				React.createElement(
+					"td",
+					{ className: "description", "data-toggle": "collapse", href: '#recipe-' + props.recipe.id, "aria-expanded": "false" },
+					props.recipe.description
+				),
+				React.createElement(
+					"td",
+					{ className: "date", "data-toggle": "collapse", href: '#recipe-' + props.recipe.id, "aria-expanded": "false" },
+					props.recipe.date
+				),
+				React.createElement(
+					"td",
+					{ className: "edit" },
+					React.createElement(
+						"button",
+						{ type: "button", className: "btn btn-warning", "data-toggle": "modal", "data-target": "#editModal", "data-recipe": props.recipe.id },
+						"Edit"
+					)
+				),
+				React.createElement(
+					"td",
+					{ className: "delete" },
+					React.createElement(
+						"button",
+						{ type: "button", className: "btn btn-danger", "data-recipe": props.recipe.id },
+						"Delete"
+					)
+				)
+			),
+			React.createElement(
+				"tr",
+				{ className: 'recipe-' + props.recipe.id },
+				React.createElement(
+					"td",
+					{ colSpan: "6" },
+					React.createElement(
+						"div",
+						{ className: "collapse recipe", id: 'recipe-' + props.recipe.id },
+						React.createElement(
+							"div",
+							{ className: "col-50" },
+							React.createElement(
+								"div",
+								{ className: "instructions" },
+								React.createElement(
+									"h3",
+									null,
+									"Instructions"
+								),
+								props.recipe.instructions
+							)
+						),
+						React.createElement(
+							"div",
+							{ className: "col-50" },
+							React.createElement(
+								"div",
+								{ className: "ingredients" },
+								React.createElement(
+									"h3",
+									null,
+									"Ingredients"
+								),
+								React.createElement(
+									"ul",
+									null,
+									ingredientList
+								)
+							)
+						),
+						React.createElement("div", { className: "clearfix" })
+					)
+				)
+			)
+		)
+	);
+}
+
+function Ingredients(props) {
+	var recipes = props.recipes;
+
+	return React.createElement(
+		"div",
+		null,
+		recipes.map(function (recipe, index) {
+			return React.createElement(Ingredient, { key: recipe.id,
+				recipe: recipe,
+				index: index + 1 });
+		})
+	);
+}
 
 var App = function (_React$Component) {
 	_inherits(App, _React$Component);
@@ -17,21 +138,35 @@ var App = function (_React$Component) {
 		var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
 
 		_this.state = {
-			recipes: []
+			recipes: [{
+				id: 1,
+				name: 'Recipe 1',
+				date: '01/10/2017',
+				description: 'This is a description',
+				instructions: 'These are the instructions',
+				ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3']
+			}, {
+				id: 2,
+				name: 'Recipe 2',
+				date: '01/11/2017',
+				description: 'This is a description 2',
+				instructions: 'These are the instructions about something else',
+				ingredients: ['Ingredient 11', 'Ingredient 21', 'Ingredient 31']
+			}]
 		};
 		return _this;
 	}
 
 	_createClass(App, [{
-		key: 'componentWillMount',
+		key: "componentWillMount",
 		value: function componentWillMount() {}
 	}, {
-		key: 'handleClick',
+		key: "handleClick",
 		value: function handleClick() {}
 	}, {
-		key: 'render',
+		key: "render",
 		value: function render() {
-			return React.createElement('div', null);
+			return React.createElement(Ingredients, { recipes: this.state.recipes });
 		}
 	}]);
 
--- src-files/scripts/controller.js
+++ src-files/scripts/controller.js
@@ -1,9 +1,81 @@
 
+function Ingredient(props) {
+	var ingredientList = [];
+	for (var i=0; i < props.recipe.ingredients.length; i++) {
+	    ingredientList.push(<li key={i}>{props.recipe.ingredients[i]}</li>);
+	}
+
+	return <table className="table table-striped">
+			<tbody>
+				<tr role="button" className={'recipe-'+props.recipe.id}>
+					<td className="index" data-toggle="collapse" href={'#recipe-'+props.recipe.id} aria-expanded="false">{props.index}</td>
+					<td className="name" data-toggle="collapse" href={'#recipe-'+props.recipe.id} aria-expanded="false">{props.recipe.name}</td>
+					<td className="description" data-toggle="collapse" href={'#recipe-'+props.recipe.id} aria-expanded="false">{props.recipe.description}</td>
+					<td className="date" data-toggle="collapse" href={'#recipe-'+props.recipe.id} aria-expanded="false">{props.recipe.date}</td>
+					<td className="edit"><button type="button" className="btn btn-warning" data-toggle="modal" data-target="#editModal" data-recipe={props.recipe.id}>Edit</button></td>
+					<td className="delete"><button type="button" className="btn btn-danger" data-recipe={props.recipe.id}>Delete</button></td>
+				</tr>
+				<tr className={'recipe-'+props.recipe.id}>
+					<td colSpan="6">
+						<div className="collapse recipe" id={'recipe-'+props.recipe.id}>
+							<div className="col-50">
+								<div className="instructions">
+									<h3>Instructions</h3>
+									{props.recipe.instructions}
+								</div>
+							</div>
+							<div className="col-50">
+								<div className="ingredients">
+									<h3>Ingredients</h3>
+									<ul>
+										{ingredientList}
+									</ul>
+								</div>
+							</div>
+							<div className="clearfix"></div>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>;
+
+
+}
+
+function Ingredients(props) {
+	const recipes = props.recipes;
+
+	return <div>
+			{recipes.map((recipe, index) =>
+				<Ingredient key={recipe.id}
+					recipe={recipe}
+					index={index+1} />
+				)}
+			</div>;
+}
+
 class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			recipes: [],
+			recipes: [
+				{
+					id: 1,
+					name: 'Recipe 1',
+					date: '01/10/2017',
+					description: 'This is a description',
+					instructions: 'These are the instructions',
+					ingredients: ['Ingredient 1','Ingredient 2','Ingredient 3']
+				},
+				{
+					id: 2,
+					name: 'Recipe 2',
+					date: '01/11/2017',
+					description: 'This is a description 2',
+					instructions: 'These are the instructions about something else',
+					ingredients: ['Ingredient 11','Ingredient 21','Ingredient 31']
+				}
+			],
 		}
 	}
 
@@ -16,11 +88,7 @@ class App extends React.Component {
 	}
 
 	render() {
-		return (
-			<div>
-
-			</div>
-		);
+		return <Ingredients recipes={this.state.recipes} />;
 	}
 }
 
